3b730f7c7e922d02e122174f5d536244f5786531
Kianoush Dortaj <8811020+Kianoush-Dortaj@users.noreply.github.com>
2023-02-22 09:24:24 +0000

    add comment for bubble sort codes

--- sort/src/bubble_sort.rs
+++ sort/src/bubble_sort.rs
@@ -1,26 +1,44 @@
 use std::fmt::Debug;
 
+// Define a function called `bubble_sort` that takes a mutable slice of generic type `T`
+// where `T` must implement the `PartialOrd` and `Debug` traits.
 pub fn bubble_sort<T: PartialOrd + Debug> (v:&mut [T]) {
 
+    // Loop through each position `p` in the slice `v`.
     for p in 0..v.len() {
+
+        // Assume that the slice is already sorted.
         let mut stored = true;
+
+        // Loop through each pair of adjacent elements in the slice `v`, up to the 
+        // position `v.len() - 1 - p` (which is where the largest element is expected to
+        // be after each iteration of the outer loop).
         for i in 0..(v.len() -1 ) - p {
+
+            // If the current element is greater than the next element, swap them.
+            // Also set the `stored` flag to `false` to indicate that the slice is not
+            // yet fully sorted.
             if v[i] > v[i+1] {
                 v.swap(i,i+1);
                 stored = false;
             }
         }
 
+        // If the `stored` flag is still `true` (i.e., the slice is already sorted), 
+        // then exit the loop early.
         if stored {
             return;
         }
     }
 }
 
+// Define some tests for the `bubble_sort` function.
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
+    // Test that `bubble_sort` can sort a vector of integers.
     fn test_bubble_sort() {
         let mut v = vec![4,6,1,8,11,13,3];
         bubble_sort(&mut v);
